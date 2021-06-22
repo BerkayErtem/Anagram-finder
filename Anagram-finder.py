3a34339791576e9722b099de2e6ac5e6f2062e8e
BerkayErtem <berkay_ertem@hotmail.com>
2021-06-22 10:55:05 +0300

    Update Anagram-finder.py

--- Anagram-finder.py
+++ Anagram-finder.py
@@ -1,39 +1,44 @@
 from collections import Counter
-count1=Counter()
-count2=Counter()
+def inputs():
+    count1=Counter()
+    count2=Counter()
 
 
-#inputs here
-sen1=input('First Sentence: ')
-sen2=input('Second Sentence: ')
+    #inputs here
+    sen1=input('First Sentence: ')
+    sen2=input('Second Sentence: ')
 
-#lower the sentences and remove the spaces
-sen1=sen1.lower()
-sen2=sen2.lower()
-sen1=sen1.replace(" ","")
-sen2=sen2.replace(" ","")
+    #lower the sentences and remove the spaces
+    sen1=sen1.lower()
+    sen2=sen2.lower()
+    sen1=sen1.replace(" ","")
+    sen2=sen2.replace(" ","")
 
-letter1=list(sen1)
-letter2=list(sen2)
+    letter1=list(sen1)
+    letter2=list(sen2)
 
-for word in letter1:
-    count1[word]+=1
-print(count1)
+    for word in letter1:
+        count1[word]+=1
+    print(count1)
 
-for word in letter2:
-    count2[word]+=1
-print(count2)
-print('-----------------')
+    for word in letter2:
+        count2[word]+=1
+    print(count2)
+    print('-----------------')
 
-if count1==count2:
-    print('They are anagram')
+    if count1==count2:
+        print('They are anagram')
+        
 
-if (count1-count2)!=0:
-    if Counter!=0:
-        print('These letters should be deleted from the FIRST sentence as many as it shows on the right side')
-        print(count1-count2)
+    elif (count1-count2)!=0:
+        if Counter!=0:
+            print('These letters should be deleted from the FIRST sentence as many as it shows on the right side')
+            print(count1-count2)
+            inputs()
+    elif (count2-count1)!=0:
 
-if (count2-count1)!=0:
+        print('These letters should be deleted from the SECOND sentence as many as it shows on the right side')
+        print(count2-count1)
+        inputs()
 
-    print('These letters should be deleted from the SECOND sentence as many as it shows on the right side')
-    print(count2-count1)
+inputs()
